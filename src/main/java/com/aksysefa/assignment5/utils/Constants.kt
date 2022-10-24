ae90e997db85321ac56e0ec36bf8d6f02f655032
aksysefa <109558170+aksysefa@users.noreply.github.com>
2022-10-25 02:43:30 +0300

    Delete src/main/java/com/aksysefa/assignment5 directory

--- src/main/java/com/aksysefa/assignment5/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.aksysefa.assignment5.utils
-
-
-object Constants {
-    const val TABLE_NAME = "PostsDatabase"
-    const val TABLE_POST_NAME = "post"
-    const val USER_TABLE_NAME = "UserDatabase"
-    const val TABLE_USER_NAME = "user"
-}
